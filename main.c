3c8646a682bd051af5166d58fea3c6d6a3503fe4
pawel.mat17@op.pl <pawel.mat17@op.pl>
2019-03-29 18:23:35 +0100

    First imoportant commit without anything

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include"headers/test.h"// tu doajesz headera i ditara siema 
 
-int main(){
 
-pisanie();
+
+int main(int ArgNum,char* Arg[] ){// faza pierwsza- napisać program w mainie, który po jednym odpaleniu jest w stanie skopiować/usuwać
+            // ogólnie zzynchronizować dwa foldery
+
+
+
+
+
 
 
 return 0;
