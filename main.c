4a35713d511c69bbd3eb4d307bbe7bd704d33438
pabblo <pawel.mat17@op.pl>
2019-03-29 17:37:07 +0100

    no kommicik z terminala

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-
+#include"headers/test.h"
 
 int main(){
 
-
+pisanie();
 
 
 return 0;
--- source/test.c
+++ source/test.c
@@ -1,7 +1,7 @@
 // to pliiczki ze wszystkimi ciałami funcji 
 // na przykład
-
+#include"../headers/test.h"
 
 void piasanie(){
-    
+    printf("no siemka z testu");
 }
